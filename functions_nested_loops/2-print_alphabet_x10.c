dc29fdf9df423460e1f1683c05fe4558d34bcd94
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-19 21:48:43 -0400

    Task2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -7,19 +7,19 @@
  */
 void print_alphabet_x10(void)
 {
-    int i;
-    char c;
+	int i;
+	char c;
 
-    i = 0;
-    while (i < 10)
-    {
-        c = 'a';
-        while (c <= 'z')
-        {
-            _putchar(c);
-            c++;
-        }
-        _putchar('\n');
-        i++;
-    }
+	i = 0;
+	while (i < 10)
+	{
+		c = 'a';
+		while (c <= 'z')
+		{
+			_putchar(c);
+			c++;
+		}
+		_putchar('\n');
+		i++;
+	}
 }

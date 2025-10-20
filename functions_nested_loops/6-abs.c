533006acc320b292a292634ff4a552b841a941a1
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-19 22:21:10 -0400

    Task 6

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -2,12 +2,13 @@
 
 /**
  * _abs - computes the absolute value of an integer
- * @n: integer input
- * Return: absolute value of n
+ * @n: the integer to check
+ *
+ * Return: the absolute value of n
  */
 int _abs(int n)
 {
-    if (n < 0)
-        return (-n);
-    return (n);
+	if (n < 0)
+		return (-n);
+	return (n);
 }

fe2b7acd884974d6c6e2b0129a57113e3685c801
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-19 21:58:10 -0400

    Task 3

--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,12 +1,23 @@
 #include "main.h"
 
 /**
- * main - check the code
+ * main - check the code for print_alphabet_x10 and _islower
  *
  * Return: Always 0
  */
 int main(void)
 {
-    print_alphabet_x10();
-    return (0);
+	int r;
+
+	print_alphabet_x10();
+
+	r = _islower('H');
+	_putchar(r + '0');
+	r = _islower('o');
+	_putchar(r + '0');
+	r = _islower(108);
+	_putchar(r + '0');
+	_putchar('\n');
+
+	return (0);
 }

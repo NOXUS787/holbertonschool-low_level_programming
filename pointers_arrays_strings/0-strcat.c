7f806763ecbaa34a4649508e5d6f61abd340d170
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-22 11:16:43 -0400

    Task 0

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -10,18 +10,9 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int i = 0, j = 0;
+	int i = 0, j = 0;
 
-    while (dest[i] != '\0')
-        i++;
+	while (dest[i] != '\0')
+		i++;
 
-    while (src[j] != '\0')
-    {
-        dest[i] = src[j];
-        i++;
-        j++;
-    }
-
-    dest[i] = '\0';
-    return (dest);
-}
+	w

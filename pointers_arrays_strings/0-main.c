3289099ba15157d1dfb7d116fda088a74428049c
NOXUS787 <alexander.rmn89@gmail.com>
2025-10-22 11:15:28 -0400

    Task 0

--- pointers_arrays_strings/0-main.c
+++ pointers_arrays_strings/0-main.c
@@ -1,18 +1,40 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
- * main - check the code
+ * _puts - prints a string followed by a new line using _putchar
+ * @str: string to print
+ */
+void _puts(char *str)
+{
+    int i = 0;
+
+    while (str[i] != '\0')
+    {
+        _putchar(str[i]);
+        i++;
+    }
+    _putchar('\n');
+}
+
+/**
+ * main - test _strcat function
  *
- * Return: Always 0.
+ * Return: 0
  */
 int main(void)
 {
-	int n;
+    char s1[98] = "Hello ";
+    char s2[] = "World!";
+    char *ptr;
 
-	n = 402;
-	printf("n=%d\n", n);
-	reset_to_98(&n);
-	printf("n=%d\n", n);
-	return (0);
+    _puts(s1);
+    _puts(s2);
+
+    ptr = _strcat(s1, s2);
+
+    _puts(s1);
+    _puts(s2);
+    _puts(ptr);
+
+    return (0);
 }
---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _strcat - Concatenates the string pointed to by src
+ *           to the end of the string pointed to by dest
+ * @dest: The destination string
+ * @src: The source string
+ *
+ * Return: Pointer to the resulting string dest
+ */
+char *_strcat(char *dest, char *src)
+{
+    int i = 0, j = 0;
+
+    while (dest[i] != '\0')
+        i++;
+
+    while (src[j] != '\0')
+    {
+        dest[i] = src[j];
+        i++;
+        j++;
+    }
+
+    dest[i] = '\0';
+    return (dest);
+}

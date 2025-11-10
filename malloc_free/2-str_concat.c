93eff35cc883b4637a4ad7dec814983084e16e66
NOXUS787 <alexander.rmn89@gmail.com>
2025-11-09 22:53:26 -0400

    Task 2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -30,3 +30,12 @@ char *str_concat(char *s1, char *s2)
         return (NULL);
 
     for (i = 0; i < len1; i++)
+        concat[i] = s1[i];
+
+    for (j = 0; j < len2; j++)
+        concat[i + j] = s2[j];
+
+    concat[i + j] = '\0';
+
+    return (concat);
+}

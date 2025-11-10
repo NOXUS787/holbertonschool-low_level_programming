2d30b7df649f04a4f29b0a161058884c9c4d02e5
NOXUS787 <alexander.rmn89@gmail.com>
2025-11-09 23:00:48 -0400

    Task 3

---
+++ malloc_free/3-main.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the 2 dimensional array
+ * @width: width of the grid
+ * @height: height of the grid
+ */
+void print_grid(int **grid, int width, int height)
+{
+    int i, j;
+
+    for (i = 0; i < height; i++)
+    {
+        for (j = 0; j < width; j++)
+            printf("%d ", grid[i][j]);
+        printf("\n");
+    }
+}
+
+/**
+ * main - check the code for alloc_grid
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int **grid;
+    int i;
+
+    grid = alloc_grid(6, 4);
+    if (grid == NULL)
+        return (1);
+
+    print_grid(grid, 6, 4);
+    printf("\n");
+
+    grid[0][3] = 98;
+    grid[3][4] = 402;
+
+    print_grid(grid, 6, 4);
+
+    /* free allocated memory */
+    for (i = 0; i < 4; i++)
+        free(grid[i]);
+    free(grid);
+
+    return (0);
+}

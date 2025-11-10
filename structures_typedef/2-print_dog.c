2abb71c1bef93474ffea05d6c63f041012dea1bf
NOXUS787 <alexander.rmn89@gmail.com>
2025-11-09 23:38:24 -0400

    Task 2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -1,16 +1,20 @@
-#include "main.h"
 #include <stdio.h>
+#include "dog.h"
 
 /**
- * print_dog - prints information of a dog
- * @d: pointer to struct dog
+ * print_dog - prints a struct dog
+ * @d: pointer to struct dog to print
+ *
+ * Description: Prints the name, age, and owner of the dog.
+ * If the pointer d is NULL, the function does nothing.
+ * If name or owner is NULL, prints "(nil)" instead.
  */
 void print_dog(struct dog *d)
 {
-    if (d == NULL)
-        return;
+	if (d == NULL)
+		return;
 
-    printf("Name: %s\n", d->name ? d->name : "(nil)");
-    printf("Age: %f\n", d->age);
-    printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
+	printf("Name: %s\n", d->name ? d->name : "(nil)");
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner ? d->owner : "(nil)");
 }
--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,25 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-#include <stddef.h>
-
 /**
- * struct dog - represents a dog
- * @name: the name of the dog
- * @age: the age of the dog
- * @owner: the owner of the dog
+ * struct dog - information about a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: Struct to store basic information about a dog.
  */
 struct dog
 {
-    char *name;
-    float age;
-    char *owner;
+	char *name;
+	float age;
+	char *owner;
 };
 
-/* Typedef for struct dog */
-typedef struct dog dog_t;
-
-/* Function prototype */
-void init_dog(struct dog *d, char *name, float age, char *owner);
+/* Function prototypes */
+void print_dog(struct dog *d);
 
 #endif /* DOG_H */

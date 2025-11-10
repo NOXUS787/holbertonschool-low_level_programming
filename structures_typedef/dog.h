c6aa7df4e946488e7cf88f681de8938a23efd46e
NOXUS787 <alexander.rmn89@gmail.com>
2025-11-09 23:25:26 -0400

    Fix: add 1-init_dog.c

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,17 +1,25 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stddef.h>
+
 /**
- * struct dog - Structure representing a dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
+ * struct dog - represents a dog
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: the owner of the dog
  */
 struct dog
 {
-	char *name;
-	float age;
-	char *owner;
+    char *name;
+    float age;
+    char *owner;
 };
+
+/* Typedef for struct dog */
+typedef struct dog dog_t;
+
+/* Function prototype */
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif /* DOG_H */

9707f75eb054cfe9efd8998f0c425f7b7f076209
NOXUS787 <alexander.rmn89@gmail.com>
2026-01-07 17:53:19 -0400

    Task 5

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -44,4 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key,
 /* Task 4 */
 char *hash_table_get(const hash_table_t *ht, const char *key);
 
+/* Task 5 */
+void hash_table_print(const hash_table_t *ht);
+
 #endif /* HASH_TABLES_H */

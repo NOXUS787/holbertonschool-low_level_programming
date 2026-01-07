f2f21fad915cef23744255b71a27a88c36a144f4
NOXUS787 <alexander.rmn89@gmail.com>
2026-01-07 17:46:09 -0400

    Task 4

--- hash_tables/hash_tables.h
+++ hash_tables/hash_tables.h
@@ -41,4 +41,7 @@ unsigned long int key_index(const unsigned char *key,
 int hash_table_set(hash_table_t *ht, const char *key,
 	const char *value);
 
+/* Task 4 */
+char *hash_table_get(const hash_table_t *ht, const char *key);
+
 #endif /* HASH_TABLES_H */
